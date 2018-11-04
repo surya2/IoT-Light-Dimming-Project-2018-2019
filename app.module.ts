6d6dc5a5c8474796664a4f019e57ca14a7c2721e
surya2 <surya.selvam03@gmail.com>
2018-11-04 13:15:29 -0500

    Initial Upload of Light Dimming Project
    These files include the code for the user application interface.

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+import { ControlComponent } from './control/control.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ControlComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
