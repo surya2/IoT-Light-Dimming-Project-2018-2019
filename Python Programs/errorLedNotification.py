25cee85bdab6a37e50ab302d8a1be6c63907b8b2
surya2 <surya.selvam03@gmail.com>
2018-12-02 14:02:18 -0500

    Update on program
    This program is the LED error notification program which will notify the user when there are error messages present in the system.

--- Python Programs/errorLedNotification.py
+++ Python Programs/errorLedNotification.py
@@ -1,14 +1,14 @@
-import RPi.GPIO as GPIO
-import time
-import os
-GPIO.setmode(GPIO.BCM)
+import RPi.GPIO as GPIO     #import Raspberry Pi hardware pins
+import time                 #import time for delays
+import os                   #import operating system functions
+GPIO.setmode(GPIO.BCM)      
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print "LED on"
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(18,GPIO.LOW)
-state = os.stat('logs/cronlog').st_size==0
-if(state == False):
-    GPIO.output(18,GPIO.HIGH)
+GPIO.setup(18,GPIO.OUT)     #look for pin 18 which is the pin that the led is connected to
+print "LED on"            
+GPIO.output(18,GPIO.HIGH)   #Turn the led on
+time.sleep(1)               #Delay 1 sec
+print "LED off"            
+GPIO.output(18,GPIO.LOW)    #Turn the led off
+state = os.stat('logs/cronlog').st_size==0      #read error message file called cronlog
+if(state == False):                  #if cronlog's size is not equal to 0
+    GPIO.output(18,GPIO.HIGH)        #Turn the led on - this is the notification
