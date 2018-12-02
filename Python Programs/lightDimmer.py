a1a0ba081c7eef6fd1774622420f1d4473a68ce5
surya2 <surya.selvam03@gmail.com>
2018-12-02 13:56:57 -0500

    lightDimmer.py Update

--- Python Programs/lightDimmer.py
+++ Python Programs/lightDimmer.py
@@ -1,23 +1,66 @@
-from firebase import firebase
-import serial
-import time
+from firebase import firebase #import firebase library on Raspberry Pi
+import serial  #import Raspberry Pi serial library to send data to Arduino
+import time #import time library to make small delays
 
-firebase = firebase.FirebaseApplication('https://basementautomation.firebaseio.com/')
-ser = serial.Serial('/dev/ttyACM0', 9600)
+firebase = firebase.FirebaseApplication('https://basementautomation.firebaseio.com/') #HTTP GET request to database
+ser = serial.Serial('/dev/ttyACM0',9600)    #Serial communication with Arduino
 
-print("Starting Program… ")
 
-firstValue = firebase.get('HomeTheater/Controls/center', None)
-print("First Intensity Value for Lights: " + str(firstValue))
+print("Hello World - Starting Program")
+
+firstCenter = firebase.get('HomeTheater/Controls/center',None)  #Getting data from teh JSON text representation
+print("Center Lights: " +   str(firstCenter))
+firstCorner = firebase.get('HomeTheater/Controls/corner',None)
+print("Corner Lights: " + str(firstCorner))
+firstStrip = firebase.get('HomeTheater/Controls/strip',None)
+print("Strip Lights: " + str(firstStrip))
+
+firstCen = str(firstCenter)
+m = str.encode(firstCen)
+type(m)
+ser.write(m)
+
+"""def nextStep(center, corner, strip):
+    centerNum = str(center)
+    cornerNum = str(corner)
+    stripNum = str(strip)
+    if center != firstCenter:
+        print('Change: ' + ' - ' + centerNum)"""
+
 
 while True:
-  nextValue = firebase.get('HomeTheater/Controls/center', None)
+    center = firebase.get('HomeTheater/Controls/center',None)
+    corner = firebase.get('HomeTheater/Controls/corner',None)
+    strip = firebase.get('HomeTheater/Controls/strip',None)
+    #nextStep(center, corner, strip)
+ if center != firstCenter:    #If the is a change in the database, do the following below.                                      
+        print('Change in Center Lights: ' + str(firstCenter) + ' - ' + str(center))
+        firstCenter = center
+        nextNum = 300 + int(center)
+        string = str(nextNum)
+        #time.sleep(5)
+        message = str.encode(string)  #encode data into byte packet
+        type(message)   #compress data
+        ser.write(message)    #send data thrpough Serial port
+        print("Sent")
+        
+#The Below Code is for future updates
+    if corner != firstCorner:
+        print('Change in Corner Lights: ' + str(firstCorner) + ' - ' + str(corner))
+        firstCorner = corner
+        if corner == 'OFF':
+            string1 = "+"
+            message = str.encode(string1)
+            type(message)
+            ser.write(message)
+            print("off")
+        if corner == 'ON+':
+            string1 = "+"
+            message = str.encode(string1)
+            type(message)
+            ser.write(message)
+            print("on")
 
-  if nextValue != firstValue:
-    print('Change in Light Brightness: ' + str(firstValue) + ' - ' + str(nextValue))
-    firstValue = nextValue
-    string = str(nextValue)
-    message = str.encode(string)
-    type(message)
-    ser.write(message)
-    print("Sent")
+    if strip != firstStrip:
+        print('Change in Strip Lights: ' + str(firstStrip) + ' - ' + str(strip))
+        firstStrip = strip
